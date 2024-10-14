1085b728371d00b3710f5cb9a23508eabf52cbeb
ashuuu2409 <ashuchopra2409@gmail.com>
2024-10-14 09:47:16 +0530

    add file

--- day-5/script.js
+++ day-5/script.js
@@ -7,6 +7,20 @@ function display() {
     console.log("hello world!");
     
 }
-var x = 14;
-console.log("hello world",x);
-display();
+let x=12.3;
+console.log(typeof(x))
+
+let y="hello"
+console.log(typeof(y))
+let z=true;
+console.log(typeof(z))
+let obj={
+    name:"rahul",
+    age:19
+}
+console.log(tyoeof(obj));
+let arr =[1,2,3,4,5]
+console.log(typeof(arr));
+let a;
+console.log(a)
+
