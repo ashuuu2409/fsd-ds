457176a5daf6074049438558994ef670634e37ba
ashuuu2409 <ashuchopra2409@gmail.com>
2024-10-14 09:17:07 +0530

    add file

---
+++ day-5/script.js
@@ -0,0 +1,12 @@
+// function display(){
+//     alert("enter number")
+// }
+// display();
+
+function display() {
+    console.log("hello world!");
+    
+}
+var x = 14;
+console.log("hello world",x);
+display();
